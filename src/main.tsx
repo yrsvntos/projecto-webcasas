87fca69882f56c2572064be990830ce938f488af
Yanik dos Santos <yrsvntos@gmail.com>
2025-09-25 15:59:50 +0200

    feat: Adicionar o AuthProvider e o Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import {router} from './App';
 import './index.css'
-
+import AuthProvider from './context/authContext';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}  />
+    <Toaster
+      position="top-center"
+      reverseOrder={false}
+    />
+    <AuthProvider>
+      <RouterProvider router={router}  />
+    </AuthProvider>
   </StrictMode>,
 )
