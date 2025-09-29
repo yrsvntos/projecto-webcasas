e449207eecac4cf7ac9dcebc35fbbc4dc3d1ee71
Yanik dos Santos <yrsvntos@gmail.com>
2025-09-29 19:33:17 +0200

    feat: Listar os imóveis na página principal

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,75 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Container } from "../../components/container";
 import { HiLocationMarker } from "react-icons/hi";
 import { BiSolidCarGarage, BiBed, BiBath } from "react-icons/bi";
 import { MdSquareFoot } from "react-icons/md";
-import { FaRegUserCircle } from "react-icons/fa";
+import { FiHome } from "react-icons/fi";
 import { FaWhatsapp } from "react-icons/fa";
+import { getDocs, collection, query, orderBy } from "firebase/firestore";
+import { db } from "../../services/firebaseConnection";
+import { Link } from "react-router-dom";
+
+interface PropertyProps{
+    id: string;
+    name: string;
+    price: string;
+    location: string | number;
+    rooms: string;
+    type: string;
+    whatsapp: string;
+    wc: string;
+    garage: string;
+    area: string;
+    images: PropertyImageProps[];
+    uid: string;
+    
+}
+
+interface PropertyImageProps{
+    name: string;
+    uid: string;
+    url: string;
+}
 
 export function Home(){
+    const [properties, setProperties] = useState<PropertyProps[]>([]);
+    const [loadImage, setLoadImage] = useState<string[]>([]);
     const [input, setInput] = useState("");
 
+    useEffect(() => {
+        async function loadProperty(){
+            const propertyRef = collection(db, "property");
+            const queryRef = query(propertyRef, orderBy("created", "desc"));
+
+            getDocs(queryRef)
+            .then((snapshot) => {
+                let listProperty = [] as PropertyProps[];
+
+                snapshot.forEach( doc => {
+                    listProperty.push({
+                        id: doc.id,
+                        name: doc.data().name,
+                        price: doc.data().price,
+                        location: doc.data().location,
+                        rooms: doc.data().rooms,
+                        type: doc.data().type,
+                        whatsapp: doc.data().whatsapp,
+                        wc: doc.data().wc,
+                        garage: doc.data().garage,
+                        area: doc.data().area,
+                        images: doc.data().images,
+                        uid: doc.data().uid
+                    })
+                })
+                setProperties(listProperty)
+            })
+        }
+        loadProperty();
+    }, [])
+
+    function handleImageLoad(id: string){
+        setLoadImage((prevImageLoaded) => [...prevImageLoaded, id])
+    }
     function handleSearchCar(){
         alert("Teste");
         return;
@@ -39,43 +100,86 @@ export function Home(){
             <main
                 className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3"
             >
-                <div className="rounded border bg-white outline-none border-zinc-100">
-                    <div className="house-img">
-                        <img 
-                            src="https://img.jamesedition.com/listing_images/2025/07/30/05/58/39/35306823-59df-4a6e-98a8-b25d652b2621/je/1100xxs.jpg" 
-                            alt="" 
-                            className="object-cover rounded-bl-none rounded-br-none rounded-md"
-                        />
-                    </div>
-                    <div className="house-info p-4">
-                        <p className="font-medium text-xl">500.000 €</p>
-                        <small className="flex items-center font-medium text-zinc-500 mt-2 gap-1">
-                            <HiLocationMarker size={16}/> Vila Nova de Gaia, Portugal
-                        </small>
-                        <div className="house-details text-sm mt-4 p-2 bg-zinc-200 gap-3 flex items-center rounded-md">
-                            <div className="rooms flex items-center gap-1">
-                                <BiBed size={23} className="text-zinc-500"/> 5
+                {properties.map(item => (
+                    <Link to={`/casas/${item.name}`}>
+                        <div 
+                            key={item.id}
+                            className="border outline-none border-zinc-100"
+                        >
+                            <div className="house-img relative">
+                            <div 
+                                className="w-full  relative h-50 rounded-lg bg-gray-400"
+                                style={{display: loadImage.includes(item.id) ? "none" : "block"}}
+                            >
+                                <FiHome size={78} color="#fff" className="absolute top-16 left-40"/>
                             </div>
-                            <div className="rooms flex items-center gap-1">
-                                <BiBath size={23} className="text-zinc-500"/> 3
+                                <img 
+                                    src={item.images[0].url} 
+                                    alt={`Foto do Imóvel  - ${item.name} `} 
+                                    className="w-full object-cover rounded-bl-none rounded-br-none rounded-md h-50"
+                                    onLoad={() => handleImageLoad(item.id)}
+                                    style={{display: loadImage.includes(item.id) ? "block" : "none"}}
+                                />
+                                <div className="bg-red-500 text-white text-sm font-bold rounded-md py-1 px-3 type-property absolute top-3 right-3">
+                                    <small>{item.type.toLocaleUpperCase()}</small>
+                                </div>
                             </div>
-                            <div className="rooms flex items-center gap-1">
-                                <BiSolidCarGarage size={23} className="text-zinc-500"/> 1
-                            </div>
-                            <div className="rooms flex items-center gap-1">
-                                <MdSquareFoot size={23} className="text-zinc-500"/> 110m<sup>2</sup>
+                            <div className="house-info bg-white rounded-md rounded-t-none p-4">
+                                <h3 className="font-bold text-xl">{item.name}</h3>
+                                <small className="flex items-center font-medium text-zinc-500 mt-2 gap-1">
+                                    <HiLocationMarker size={16}/>{item.location}
+                                </small>
+                                <div className="house-details text-sm mt-4 p-2 bg-zinc-200 gap-3 flex items-center rounded-md">
+                                    <div className="rooms flex items-center gap-1 relative group">
+                                        <BiBed size={23} className="text-zinc-500"/> {item.rooms}
+                                        <span 
+                                            className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 hidden group-hover:block bg-gray-800 text-white text-sm rounded-lg px-3 py-1 shadow-lg" 
+                                        >
+                                            Quartos
+                                        </span>
+                                    </div>
+
+                                    <div className="rooms flex items-center gap-1 relative group">
+                                        <BiBath size={23} className="text-zinc-500"/> {item.wc}
+                                        <span 
+                                            className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 hidden group-hover:block bg-gray-800 text-white text-sm rounded-lg px-3 py-1 shadow-lg" 
+                                        >
+                                            WC
+                                        </span>
+                                    </div>
+                                    <div className="rooms flex items-center gap-1 relative group">
+                                        <BiSolidCarGarage size={23} className="text-zinc-500"/> {item.garage}
+                                        <span 
+                                            className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 hidden group-hover:block bg-gray-800 text-white text-sm rounded-lg px-3 py-1 shadow-lg" 
+                                        >
+                                            Garagem
+                                        </span>
+                                    </div>
+                                    <div className="rooms flex items-center gap-1 relative group">
+                                        <MdSquareFoot size={23} className="text-zinc-500"/> {item.area}m<sup>2</sup>
+                                        <span 
+                                            className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 hidden group-hover:block bg-gray-800 text-white text-sm rounded-lg px-3 py-1 shadow-lg" 
+                                        >
+                                            Área Bruta
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="house-vendor flex items-center justify-between mt-4 text-sm">
+                                    <div className="vendor-name flex items-center gap-1">
+                                        <p className="font-medium text-md text-zinc-900"><strong>MZN {item.price}</strong></p>
+                                    </div>
+                                    <a 
+                                        href={`https://api.whatsapp.com/send?phone=${item.whatsapp}`} 
+                                        target="_blank"
+                                        className="bg-green-600 vendor-contact border flex items-center text-white rounded outline px-2 py-1 gap-1 cursor-pointer">
+                                        <FaWhatsapp size={18}/> Contactar
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                        <div className="house-vendor flex items-center justify-between mt-4 text-sm">
-                            <div className="vendor-name flex items-center gap-1">
-                                <FaRegUserCircle size={20} className="text-zinc-500"/> Yanik dos Santos
-                            </div>
-                            <div className="bg-green-600 vendor-contact border flex items-center text-white rounded outline px-2 py-1 gap-1 cursor-pointer">
-                                <FaWhatsapp size={18}/> Contactar
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    </Link>
+                ))}
+                
             </main>
         </Container>
     );
