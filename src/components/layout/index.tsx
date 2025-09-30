9c5a5c8eb5d38bdf52e40b5af4e4aba2112730e6
Yanik dos Santos <yrsvntos@gmail.com>
2025-09-30 13:26:55 +0200

    feat: Adcionar o componente footer

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,13 @@
 import Header from '../header/index';
 import { Outlet } from 'react-router-dom';
+import Footer from '../footer';
 
 export default function Layout(){
     return(
         <>
             <Header/>
             <Outlet/>
+            <Footer/>
         </>
     );
 }
