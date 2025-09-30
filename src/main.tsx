d255c91dac0c43ea7e64ab3a75cf465ccdccf773
Yanik dos Santos <yrsvntos@gmail.com>
2025-09-30 13:40:39 +0200

    style: importar estilos do Swiper no main

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import {router} from './App';
 import './index.css'
 import AuthProvider from './context/authContext';
 import { Toaster } from 'react-hot-toast';
+import {register} from 'swiper/element/bundle';
+
+register();
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
