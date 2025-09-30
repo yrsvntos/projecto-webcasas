a078e75e448bdbee3bb58f418f265e1df6066ade
Yanik dos Santos <yrsvntos@gmail.com>
2025-09-30 13:27:57 +0200

    feat: Criar o componente Footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from "../container";
+
+export default function Footer(){
+    return(
+        <Container>
+            <section className="border-t p-4 border-zinc-300">
+                <p className="text-zinc-800 font-medium">WebCasas 2025. Todos os direitos reservados</p>
+            </section>
+            
+        </Container>
+    )
+   
+}
